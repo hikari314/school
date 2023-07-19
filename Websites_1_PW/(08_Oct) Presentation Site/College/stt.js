9c9f96712a41865f8f3afb612b1a0c5caa750883
hikari314 <109241719+hikari314@users.noreply.github.com>
2023-07-19 14:52:36 -0300

    a

---
+++ Websites_1_PW/(08_Oct) Presentation Site/College/stt.js
@@ -0,0 +1,14 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Menu icon
+function myFunction(x) {
+  x.classList.toggle("change");
+}
